45f3b49abd239062d7ec9d01ec565bdc89c7195d
Maksim Starodubtsev <maxstardub@mail.ru>
2022-10-14 19:53:08 +0300

    Added realisation

---
+++ src/test/java/com/dmdev/integration/UnitTests.java
@@ -0,0 +1,48 @@
+package com.dmdev.integration;
+
+import com.dmdev.dto.CreateUserDto;
+import com.dmdev.dto.UserDto;
+import com.dmdev.entity.Gender;
+import com.dmdev.entity.Role;
+import com.dmdev.entity.User;
+import com.dmdev.exception.ValidationException;
+import com.dmdev.mapper.CreateUserMapper;
+import com.dmdev.mapper.UserMapper;
+import com.dmdev.util.LocalDateFormatter;
+import com.dmdev.validator.Error;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.annotation.Testable;
+
+import java.util.List;
+
+@Testable
+public class UnitTests {
+
+    @Test
+    public void checkEqualityOfUsers0(){
+        User user0 = new User();
+        Assertions.assertTrue(user0.equals(user0));
+    }
+
+    @Test
+    public void checkEqualityOfUsersToNull(){
+        User user0 = new User();
+        Assertions.assertFalse(user0.equals(null));
+    }
+
+    @Test
+    public void checkEqualityOfUsersDto(){
+        UserDto userDto1 = UserDto.builder().build();
+        UserDto userDto0 = userDto1;
+        Assertions.assertTrue(userDto1.equals(userDto0));
+    }
+
+    @Test
+    public void checkValidateExceptionClass(){
+        Error error = Error.of("Name", "Error");
+        ValidationException validationException = new ValidationException(List.of(error));
+        Assertions.assertEquals(validationException.getErrors().toArray()[0],
+                error);
+    }
+}
